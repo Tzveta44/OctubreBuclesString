bc0dc2f2453f4641dd72b39a29e0955891cc2596
BTorija <beatriz_torija@hotmail.com>
2023-10-18 16:15:03 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("PETKOVA");
+
+        String nombre = "Manolo";
+        System.out.println(nombre.toLowerCase());
+        System.out.println(nombre.toUpperCase());
+        System.out.println(nombre);
+        System.out.println(nombre.isEmpty());
+        System.out.println(nombre.indexOf("n"));
+        System.out.println(nombre.indexOf("o"));
+        System.out.println(nombre.lastIndexOf("o"));
+        System.out.println(nombre.substring(4));
+        System.out.println(nombre.substring(2, 4));
+        System.out.println(nombre.length());
+
+        //Dada la cuenta bancaria 1234567890123456789
+        //Se pide extraer los primeros dos digitos
+        //Se pide extraer los 10 ultimos
+        //Se pide extraer de la posicion 4 a la 9
     }
 }
